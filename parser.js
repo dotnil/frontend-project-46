1b822f03457c00b31b71eb89bcff8138d219890b
Tatiana Kh <ts.khol@gmail.com>
2024-08-16 21:45:48 +0300

    add flat file comparison (JSON)

--- parser.js
+++ parser.js
@@ -2,8 +2,9 @@ import fs from 'fs';
 
 const readFile = (filepath) => {
   try {
-    const data = fs.readFileSync(filepath, 'utf8');
-    console.log('Содержимое файла:', JSON.parse(data));
+    const content = fs.readFileSync(filepath, 'utf8');
+
+    return JSON.parse(content);
   } catch (err) {
     console.error('Ошибка чтения файла:', err);
   }
