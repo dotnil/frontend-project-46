742c02881f4aa160a6c45366ae3983c9ceff5d70
Tatiana Kh <ts.khol@gmail.com>
2024-08-26 17:51:44 +0300

    add test

--- parser.js
+++ parser.js
@@ -1,13 +1,19 @@
 import fs from 'fs';
+import yaml from 'js-yaml';
+
+const determineFormat = (filepath) => {
+  const fileParts = filepath.split('.');
+  return fileParts[fileParts.length - 1];
+};
 
 const readFile = (filepath) => {
-  try {
-    const content = fs.readFileSync(filepath, 'utf8');
+  const format = determineFormat(filepath);
+  const content = fs.readFileSync(filepath, 'utf8');
+  console.log(format);
 
-    return JSON.parse(content);
-  } catch (err) {
-    console.error('Ошибка чтения файла:', err);
-  }
+  if (format === 'json') return JSON.parse(content);
+  if (format === 'yml') return yaml.load(content);
+  throw new Error('Unsupported file format or invalid data');
 };
 
 export default readFile;
