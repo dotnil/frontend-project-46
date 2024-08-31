ec4312a8e7097929b05da83e566b7bc77305be3c
Tatiana Kh <ts.khol@gmail.com>
2024-08-31 19:18:25 +0300

    upload coverage to code climate

--- src/gendiff.js
+++ src/gendiff.js
@@ -11,22 +11,24 @@ const isNotChanged = (value1, value2) => value1 === value2;
 const isDeleted = (value2) => value2 === undefined;
 const isAdded = (value1) => value1 === undefined;
 
-const renderLines = (keys, object1, object2) => {
-  const lines = keys.map((key) => {
-    const value1 = object1[key];
-    const value2 = object2[key];
-    if (isNotChanged(value1, value2)) {
-      return `    ${key}: ${value1}`;
-    }
-    if (isDeleted(value2)) {
-      return `  - ${key}: ${value1}`;
-    }
-    if (isAdded(value1)) {
-      return `  + ${key}: ${value2}`;
-    }
+const renderLine = (key, object1, object2) => {
+  const value1 = object1[key];
+  const value2 = object2[key];
+  if (isNotChanged(value1, value2)) {
+    return `    ${key}: ${value1}`;
+  }
+  if (isDeleted(value2)) {
+    return `  - ${key}: ${value1}`;
+  }
+  if (isAdded(value1)) {
+    return `  + ${key}: ${value2}`;
+  }
 
-    return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
-  }).join('\n');
+  return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
+};
+
+const renderLines = (keys, object1, object2) => {
+  const lines = keys.map((key) => renderLine(key, object1, object2)).join('\n');
 
   return `{\n${lines}\n}`;
 };
